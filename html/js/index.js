1f34db1887394171f6e0a3f0e1a4f924b37fe1a7
HectorNLeon <hectornleon98@gmail.com>
2019-11-14 20:21:30 -0600

    first commit

---
+++ html/js/index.js
@@ -0,0 +1,83 @@
+function watchForm(){
+    loadList();
+    $("#postButton").click( function(e){
+        let postD = $(".post");
+        let body = {};
+        body.title = postD[0].value;
+        body.content = postD[1].value;
+        body.author = postD[2].value;
+        body.publishDate = postD[3].value;
+        e.preventDefault();
+        $.ajax({
+            type : 'POST',
+            url: 'http://localhost:8080/blog-posts',
+            contentType: "application/json",
+            data: JSON.stringify(body)
+        }).done(function(data){
+            console.log(data);
+            loadList();
+        }).fail(function(msg){
+            alert(msg.responseText);
+        });
+    });
+
+    $("#delButton").click( function(e){
+        e.preventDefault();
+        if($(".del").val() == ""){
+            alert("Missing Id!");
+        }
+        else{
+            $.ajax({
+                type : 'DELETE',
+                url: 'http://localhost:8080/blog-posts/'+$(".del").val()
+            }).done(function(data){
+                console.log(data);
+                loadList();
+            }).fail(function(msg){
+                alert(msg.responseText);
+            });
+        }
+    });
+
+    $("#updateButton").click( function(e){
+        e.preventDefault();
+        let postD = $(".update");
+        let body = {};
+        body.id = postD[0].value
+        if(body.id == "")
+            return alert("Missing Id!");
+        if(postD[1].value != "")
+            body.title = postD[1].value;
+        if(postD[2].value != "")
+            body.content = postD[2].value;
+        if(postD[3].value != "")
+            body.author = postD[3].value;
+        if(postD[4].value != "")
+            body.publishDate = postD[4].value;
+        $.ajax({
+            type : 'PUT',
+            url: 'http://localhost:8080/blog-posts/'+body.id,
+            contentType: "application/json",
+            data: JSON.stringify(body)
+        }).done(function(data){
+            console.log(data);
+            loadList();
+        }).fail(function(msg){
+            alert(msg.responseText);
+        });
+    });
+}
+
+function loadList(){
+    $("#blogList").html("");
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/blog-posts'
+    }).done(function(data){
+        for(let i=0; i<data.length; i++){
+            $("#blogList").append("<li>"+JSON.stringify(data[i])+"</li>");
+        }
+    });
+}
+
+watchForm();
---
+++ server.js
@@ -0,0 +1,177 @@
+let express = require('express');
+let morgan = require('morgan');
+let bodyParser = require('body-parser');
+var cors = require('cors');
+let jsonParser = bodyParser.json();
+let app = express();
+let moment = require('moment');
+const uuidv4 = require('uuid/v4');
+let { PostList } = require('./model');
+let { DATABASE_URL, PORT } = require('./config');
+
+
+app.use(cors());
+app.use(express.static('public'));
+app.use( morgan( 'dev' ) );
+
+let postss = [
+	{
+        id: uuidv4(),
+        title: "Yes",
+        content: "I used to know",
+        author: "Hector Leon",
+        publishDate: moment(Date.now()).format('MM/DD/YYYY')
+	},
+	{
+        id: uuidv4(),
+        title: "No",
+        content: "I know",
+        author: "Hector Leon",
+        publishDate: moment(Date.now()).format('MM/DD/YYYY')
+	},
+	{
+        id: uuidv4(),
+        title: "Maybe",
+        content: "I used",
+        author: "Hector Leon",
+        publishDate: moment(Date.now()).format('MM/DD/YYYY')
+    },
+	{
+        id: uuidv4(),
+        title: "Who knows",
+        content: "used to",
+        author: "Hector Leon",
+        publishDate: moment(Date.now()).format('MM/DD/YYYY')
+	}
+];
+
+app.get('/blog-posts',(req, res, next) => {
+    PostList.get().then(posts => {
+        return res.status(200).json(posts);
+    }).catch( error => {
+        res.statusMessage = "Something went wrong with the DB. Try again later.";
+        return res.status( 500 ).json({
+            status : 500,
+            message : "Something went wrong with the DB. Try again later."
+        })
+    });
+});
+
+app.get('/blog-post',(req, res, next) => {
+    if(req.query.author == undefined)
+        return res.status(406).json("Missing author");
+    PostList.getbyA(req.query.author).then(posts => {
+        return res.status(200).json(posts);
+    }).catch( error => {
+        res.statusMessage = "Something went wrong with the DB. Try again later.";
+        return res.status( 500 ).json({
+            status : 500,
+            message : "Something went wrong with the DB. Try again later."
+        })
+    });
+});
+
+app.post('/blog-posts', jsonParser, (req, res, next)=>{
+    if(req.body.title && req.body.content && req.body.author && req.body.publishDate){
+        let nPost = req.body;
+        nPost.id = uuidv4();
+        posts.push(nPost);
+        PostList.post(nPost).then(post => {
+            
+            return res.status(200).json({
+				message : "Post added!",
+				status : 200,
+				post : post
+			});
+        }).catch( error => {
+            res.statusMessage = "Something went wrong with the DB. Try again later.";
+            return res.status( 500 ).json({
+                status : 500,
+                message : "Something went wrong with the DB. Try again later."
+            })
+        });
+    }
+    else{
+        return res.status(406).json("Missing variables in body");
+    }
+});
+
+app.delete('/blog-posts/:id', (req, res, next)=>{
+    PostList.del(req.params.id).then(mess => {
+        return res.status(200).json("Success!");
+    }).catch( error => {
+        res.statusMessage = "Something went wrong with the DB. Try again later.";
+        return res.status( 500 ).json({
+            status : 500,
+            message : "Something went wrong with the DB. Try again later."
+        })
+    });
+    return res.status(404).send("That id doesn't exist");
+});
+
+app.put('/blog-posts/:id', jsonParser, (req, res, next)=>{
+    if(req.body.id){
+        let post = req.body;
+        if(post.id != req.params.id)
+            return res.status(409).json("Ids don't match");
+
+        PostList.update(post).then(posts => {
+            return res.status(202).json(posts);
+        }).catch( error => {
+            res.statusMessage = "Something went wrong with the DB. Try again later.";
+            return res.status( 500 ).json({
+                status : 500,
+                message : "Something went wrong with the DB. Try again later."
+            })
+        });
+    }
+    else{
+        return res.status(406).json("Missing id in body");
+    }
+});
+
+let server;
+
+function runServer(port, databaseUrl){
+	return new Promise( (resolve, reject ) => {
+		mongoose.connect(databaseUrl, response => {
+			if ( response ){
+				return reject(response);
+			}
+			else{
+				server = app.listen(port, () => {
+					console.log( "App is running on port " + port );
+					resolve();
+				})
+				.on( 'error', err => {
+					mongoose.disconnect();
+					return reject(err);
+				})
+			}
+		});
+	});
+}
+
+function closeServer(){
+	return mongoose.disconnect()
+		.then(() => {
+			return new Promise((resolve, reject) => {
+				console.log('Closing the server');
+				server.close( err => {
+					if (err){
+						return reject(err);
+					}
+					else{
+						resolve();
+					}
+				});
+			});
+		});
+}
+
+runServer( PORT, DATABASE_URL )
+	.catch( err => {
+		console.log( err );
+	});
+
+module.exports = { app, runServer, closeServer };
